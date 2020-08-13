7904d2d9333829408c9f663017f55df7e44fba7c
Niket Patel <32422309+niketp03@users.noreply.github.com>
2020-08-12 17:36:38 -0700

    added class categorization

--- TimeBudgetPlanner/src/pages/school.js
+++ TimeBudgetPlanner/src/pages/school.js
@@ -2,7 +2,42 @@ import React from "react"
 import * as CSV from 'csv-string';
 
 export default function School(data) {
-  const classes = CSV.parse(data.pageContext.data)
+  let classes = CSV.parse(data.pageContext.data)
+  classes = classes.slice(1)
+
+  const charToCat = {
+    'a' : 'History',
+    'b' : 'English',
+    'c' : 'Math',
+    'd' : 'Science',
+    'e' : 'Foreign Language',
+    'f' : 'Visual and Performing arts',
+    'g' : 'College Prep Elective',
+    'o' : 'Other'
+  }
+
+  let categorizedClasses = {
+    "History": {},
+    "English": {},
+    "Math": {},
+    "Science": {},
+    "Foreign Language": {},
+    "Visual and Performing arts": {},
+    "College Prep Elective": {},
+    "Other": {}
+  }
+  
+  for (var i = 0; i < classes.length; i++) { 
+    let class_ = classes[i]
+    let cats = class_[3].split('')
+    for (var j = 0; j < cats.length; j++) {
+      categorizedClasses[charToCat[cats[j]]][class_[0]] = [class_[1],class_[2]]
+      categorizedClasses[charToCat['o']][class_[0]] = [class_[1],class_[2]]
+    }
+  } 
+
+  console.log(categorizedClasses)
+
   return (
     <div>
       {console.log(classes)}
