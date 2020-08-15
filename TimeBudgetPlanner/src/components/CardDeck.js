7d51b2f15077c55f1448d0e595d74f3d80685f31
Sid K <sidkhillon24@gmail.com>
2020-08-14 17:53:54 -0700

    sliderOnly

--- TimeBudgetPlanner/src/components/CardDeck.js
+++ TimeBudgetPlanner/src/components/CardDeck.js
@@ -8,23 +8,16 @@ export default class CardDeck extends Component {
     render() {
         if (this.props.sliderOnly == true){
             return (
-                <div>
-                    <div class="card-deck">
-                        <InputCardSliderOnly title={this.keys[0]} min={this.props.data[this.keys[0]][0]} max={this.props.data[this.keys[0]][1]} desc={this.props.data[this.keys[0]][2]}/>
-                        <InputCardSliderOnly title={this.keys[1]} min={this.props.data[this.keys[1]][0]} max={this.props.data[this.keys[1]][1]} desc={this.props.data[this.keys[1]][2]}/>
-                        <InputCardSliderOnly title={this.keys[2]} min={this.props.data[this.keys[2]][0]} max={this.props.data[this.keys[2]][1]} desc={this.props.data[this.keys[2]][2]}/>
-                    </div>
+                <div class="card-deck">
+                    <InputCardSliderOnly title={this.keys[0]} data={this.props.data[this.keys[0]]} onChange={this.props.onChange} value={this.props.hourValues[this.keys[0]]}/>
+                    <InputCardSliderOnly title={this.keys[1]} data={this.props.data[this.keys[1]]} onChange={this.props.onChange} value={this.props.hourValues[this.keys[1]]}/>
+                    <InputCardSliderOnly title={this.keys[2]} data={this.props.data[this.keys[2]]} onChange={this.props.onChange} value={this.props.hourValues[this.keys[2]]}/>
                 </div>
             );
         }
         return (
             <div class="card-deck">
-                <InputCard 
-                    title={this.keys[0]} 
-                    classes={this.props.data[this.keys[0]]} 
-                    onChange={this.props.onChange} 
-                    value={this.props.hourValues[this.keys[0]]}
-                />
+                <InputCard title={this.keys[0]} classes={this.props.data[this.keys[0]]} onChange={this.props.onChange} value={this.props.hourValues[this.keys[0]]}/>
                 <InputCard title={this.keys[1]} classes={this.props.data[this.keys[1]]} onChange={this.props.onChange} value={this.props.hourValues[this.keys[1]]}/>
                 <InputCard title={this.keys[2]} classes={this.props.data[this.keys[2]]} onChange={this.props.onChange} value={this.props.hourValues[this.keys[2]]}/>
             </div>
