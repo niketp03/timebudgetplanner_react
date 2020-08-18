9fb4662d381cff787135f1619ce00741069bb450
Niket Patel <32422309+niketp03@users.noreply.github.com>
2020-08-18 14:04:50 -0700

    Update school.js

--- TimeBudgetPlanner/src/pages/school.js
+++ TimeBudgetPlanner/src/pages/school.js
@@ -49,7 +49,7 @@ export default function School(data) {
 
   var chartReference = {}; // Used to force chart update
   
-  const [chartState, setChartState] = useState(formatChartJSON([0,49,0,0,0,0,0,0,0,0,168])); //Set initial chart conditions
+  const [chartState, setChartState] = useState(formatChartJSON([0,49,0,0,0,0,0,0,0,0,119])); //Set initial chart conditions
 
   function formatChartJSON(chartData){
     return {
